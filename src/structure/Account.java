864a3942eb513f1f3946239d80267a79e5ea47b5
Deepak Selvaraj <sdeepak53646@gmail.com>
2024-01-02 19:08:36 +0530

    classes example

--- src/structure/Account.java
+++ src/structure/Account.java
@@ -1,4 +1,15 @@
 package structure;
 
 public class Account {
+    String owner;
+    double minimumBalance;
+    double balance;
+
+    public void deposit(double amount){
+        //implementation
+    }
+
+    public void withdraw(double amount){
+        //implementation
+    }
 }
