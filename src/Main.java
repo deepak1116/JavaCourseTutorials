357d380709ec5811e0e83ad415f14ecb6bce5b24
Deepak Selvaraj <sdeepak53646@gmail.com>
2024-01-02 19:25:20 +0530

    packages and importing classes from packages.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import structure.Account;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Account account = new Account();
+        LocalDate localDate = LocalDate.now();
+
+    }
+}
--- src/structure/Main.java
+++
@@ -1,7 +0,0 @@
-package structure;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
