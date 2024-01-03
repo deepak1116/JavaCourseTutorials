baba250c5954ce7175f41dc7319b5341ecf0e36a
Deepak Selvaraj <sdeepak53646@gmail.com>
2024-01-03 18:27:41 +0530

    Primitive variables

---
+++ src/variables/ExamplePrimitives.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class ExamplePrimitives {
+    public static void main(String[] args) {
+        byte b = 35;
+        short s = 23000;
+        int i = 35000;
+        long l = 12345678901L;
+
+        float f = 3.2f;
+        double d = 3.2121241;
+
+        char c = 'a';
+
+        boolean bool =  true;
+    }
+}
