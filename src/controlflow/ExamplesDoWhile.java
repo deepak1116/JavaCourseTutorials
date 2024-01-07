1f23fdd1cd31d5ed58734cff78af628c33307d88
Deepak Selvaraj <sdeepak53646@gmail.com>
2024-01-07 20:29:11 +0530

    control flow Do While loop

---
+++ src/controlflow/ExamplesDoWhile.java
@@ -0,0 +1,14 @@
+package controlflow;
+
+import java.util.Scanner;
+
+public class ExamplesDoWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input ;
+        do {
+            System.out.println("Enter name : ");
+            input = scanner.nextLine();
+        }while (input.length() < 2);
+    }
+}
