4c92154aac2ac4f6f96619804370467afd92636f
Deepak Selvaraj <sdeepak53646@gmail.com>
2024-01-23 19:16:05 +0530

    OOPS Polymorphism

---
+++ src/oop/polymorphism/App.java
@@ -0,0 +1,15 @@
+package oop.polymorphism;
+
+public class App {
+    public static void main(String[] args) {
+        Animal  dog = new Dog();
+        Cat cat = new Cat();
+
+        dog = new Cat();
+
+        dog.eat();
+        cat.eat();
+
+        ((Dog) dog).bark();
+    }
+}
