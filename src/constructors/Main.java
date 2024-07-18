51b5d535b40a3dd5e3d268f8ce3566045b45734c
Deepak Selvaraj <sdeepak53646@gmail.com>
2024-07-18 20:17:40 +0530

    Constructors

---
+++ src/constructors/Main.java
@@ -0,0 +1,10 @@
+package constructors;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("John",20);
+        System.out.println(s1.getAge());
+        System.out.println(s1.getName());
+
+    }
+}
